46af6548395e1e515f3742ace9d5c898171413a8
Kunal Dagar <79902312+KUNAL-DAGAR-2002@users.noreply.github.com>
2023-12-12 16:40:15 +0530

    first commit

---
+++ static/service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'your-app-cache';
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',  // Adjust this if your HTML file has a different name or is in a different directory
+        // Add other paths you want to cache
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
